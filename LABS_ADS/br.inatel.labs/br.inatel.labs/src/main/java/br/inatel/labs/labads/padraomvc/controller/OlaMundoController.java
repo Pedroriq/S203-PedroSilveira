efb53be370415f54a115bd2ecbe16102c566e2e8
Pedroriq <90358361+Pedroriq@users.noreply.github.com>
2022-11-07 22:55:59 -0300

    MVC

---
+++ LABS_ADS/br.inatel.labs/br.inatel.labs/src/main/java/br/inatel/labs/labads/padraomvc/controller/OlaMundoController.java
@@ -0,0 +1,12 @@
+package br.inatel.labs.labads.padraomvc.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OlaMundoController {
+
+    @GetMapping("/ola")
+    public String getOla(){
+        return "ola-mundo";
+    }
+}
